5030dce34c12fea41fcecdb86786d9347f434daf
zhaoyongtao <285929232@qq.com>
2018-06-13 12:51:04 +0800

    自定义layout第一步1.调用每个子view的measure(),让子view自我测量

---
+++ app/src/main/java/com/zyt/uxin/custonview/layout/OverrideLayoutView.java
@@ -0,0 +1,86 @@
+package com.zyt.uxin.custonview.layout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OverrideLayoutView extends ViewGroup {
+
+    public OverrideLayoutView(Context context) {
+        super(context);
+    }
+
+    public OverrideLayoutView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public OverrideLayoutView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private int mTotalWidth;
+    private int mTotalLength;
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        //onMeasure()的重写对于ViewGroup来说包含三部分内容
+        //1.调用每个子view的measure(),让子view自我测量
+        //2.根据子view得出的尺寸,得出子view的位置,并吧它们 的位置保存下来
+        //3.根据子view的位置和尺寸计算出自己的尺寸,并用setMeasureEimension()保存
+
+        int selfWidthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int selfWidthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int usedWidth = 0;
+
+        for (int i = 0; i < getChildCount(); i++) {
+            View childView = getChildAt(i);
+            LayoutParams lp = childView.getLayoutParams();
+//            lp.width:对应layout_width(转换过了的值)
+            //wrap_content=>WRAP_CONENT
+            // match_parent=>MATCH_PARENT
+            //xxdp/xxsp=>具体的像素值
+
+            int childWidthSpec;//测量后字view的width
+
+            switch (lp.width) {
+                case LayoutParams.MATCH_PARENT://填满父控件
+                    int 可用宽度 = 0;//可用宽度,通过widthMeasureSpec计算得到的
+                    switch (selfWidthMode) {
+                        case MeasureSpec.EXACTLY:
+                            可用宽度 = selfWidthSize;
+//                            break;
+                        case MeasureSpec.AT_MOST://和EXACTLY只有在测量自己的时候有区别
+                            可用宽度 = selfWidthSize;//也是随便你用
+                            childWidthSpec = MeasureSpec.makeMeasureSpec(selfWidthSize - usedWidth, MeasureSpec.EXACTLY);//第一个子view可以用全部可用宽度去测量,但第二个可用得减去第一个用掉的,特殊情况,第一个match_parent就把空间全部占用了
+
+                            break;
+                        case MeasureSpec.UNSPECIFIED://可用空间,无限大
+                            //这里父控件本身,match_parent,而子view又要求无限大空间,矛盾,此时将mode传UNSPECIFIED,size随便写吧,因为没用(新版android来说size也是有用的)
+                            childWidthSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+                            break;
+
+                    }
+
+                    break;
+                case LayoutParams.WRAP_CONTENT://让子view自己去测量,且不能超过父view边界
+//                    if (selfWidthMode == MeasureSpec.EXACTLY || selfWidthMode == MeasureSpec.AT_MOST) {
+                    if ((selfWidthMode & (MeasureSpec.EXACTLY | MeasureSpec.AT_MOST)) != 0) {//等同于上行
+                        childWidthSpec = MeasureSpec.makeMeasureSpec(selfWidthSize - usedWidth, MeasureSpec.AT_MOST);//第2个参数是AT_MOST,还是要让子view自己去测量的
+                    } else {//MeasureSpec.UNSPECIFIED
+                        childWidthSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+                    }
+                    break;
+                default://开发者写的固定尺寸值
+                    childWidthSpec = MeasureSpec.makeMeasureSpec(lp.width, MeasureSpec.EXACTLY);
+                    break;
+            }
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
